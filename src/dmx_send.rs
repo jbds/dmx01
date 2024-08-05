a7bd67da8147098779af911a683b1e55447c5edc
jonb <sales@jbds.co.uk>
2024-08-05 20:41:14 +0100

    refactor

--- src/dmx_send.rs
+++ src/dmx_send.rs
@@ -1,10 +1,13 @@
-pub mod dmx_send {
+use dmx::{self, DmxTransmitter};
+use std::{thread, time};
+use std::sync::mpsc::Receiver;
 
-    use std::{thread};
+
+pub fn spawn_receiver(rx: Receiver<Vec<u8>>) {
 
     thread::spawn(move || {
         let mut dmx_port = dmx::open_serial("/dev/serial0").unwrap();
-    
+
         let mut previous_data: Vec<u8> = vec![0x00, 0x00];
         
         // we want this thread to be sending packets for the lifetime of the app
@@ -21,10 +24,12 @@ pub mod dmx_send {
                 Err(_e) => {
                     dmx_port.send_dmx_packet(&previous_data).unwrap();                
                 }       
-    
+
             }
             thread::sleep(time::Duration::from_millis(50));
         }
     });
-}
 
+
+
+}
--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,13 @@
-use dmx::{self, DmxTransmitter};
-use std::{thread, time, sync};
-use dmx_send;
+mod dmx_send;
+use std::{thread, sync, time};
+
 
 fn main() {
     // data can be arbitrary number of u8 values, so use Vec<u8>
     // we do NOT use a reference, because ownership needs to pass to new thread
     let (tx, rx) = sync::mpsc::channel::<Vec<u8>>();
 
-    thread::spawn(move || {
-        let mut dmx_port = dmx::open_serial("/dev/serial0").unwrap();
-
-        let mut previous_data: Vec<u8> = vec![0x00, 0x00];
-        
-        // we want this thread to be sending packets for the lifetime of the app
-        let mut j = 0;
-        loop {
-            j += 1;
-            println!("hi number {} from the spawned thread", j);
-            
-            match rx.try_recv() {
-                Ok(data) =>  {
-                    previous_data = data.clone();
-                    dmx_port.send_dmx_packet(&data).unwrap();                
-                },
-                Err(_e) => {
-                    dmx_port.send_dmx_packet(&previous_data).unwrap();                
-                }       
-
-            }
-            thread::sleep(time::Duration::from_millis(50));
-        }
-    });
-
+    dmx_send::spawn_receiver(rx);
 
     for i in 0..100 {
         println!("hi number {} from the main thread", i);
@@ -48,5 +24,5 @@ fn main() {
         thread::sleep(time::Duration::from_millis(100));
     }
 
-    println!("finished");
+    println!("finished (with dmx_send module");
 }
